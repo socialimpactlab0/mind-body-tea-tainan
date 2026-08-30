5489857fcca7974017655bea3aed5b1debde903c
socialimpactlab0 <flt.roger@gmail.com>
2026-08-30 12:52:22 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.TEA_EVENT_CONFIG = {
+  // 部署 GAS 網頁應用程式後，把 /exec 網址貼在下方引號內。
+  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycby1Ldx8mHZjDMrsaDvM-GN7OASqQEqopKW_KWe11J0Z-AzT7IeXXkm_IcGpkYm6RDCGsg/exec",
+  EVENT_ID: "2026-09-16-mind-body-tea-tainan"
+};
